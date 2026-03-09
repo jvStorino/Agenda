282ff3d71fd447e0a702eb8318b4ae605fe351d8
jvStorino <storinojv@gmail.com>
2026-03-09 12:31:43 -0300

    Adicionando campos no models Contact

--- contact/models.py
+++ contact/models.py
@@ -3,11 +3,10 @@ from django.utils import timezone
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
-# email (email), created_date (date), description (text)
+# email (email), created_date (date), description (text), show (boolean), picture (imagem)
 
 # Depois
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# category (foreign key), owner (foreign key)
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -16,6 +15,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m', blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- project/settings.py
+++ project/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'static-root'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
