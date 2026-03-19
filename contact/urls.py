9b391afa317db55158281fd19fafd87fd37797b6
jvStorino <storinojv@gmail.com>
2026-03-19 14:12:54 -0300

    Add url/view e template para visualizar contato

--- contact/urls.py
+++ contact/urls.py
@@ -4,5 +4,6 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.single_contact, name='single_contact'),
     path('', views.index, name='contact'),
 ]
--- contact/views/contacts.py
+++ contact/views/contacts.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
-contacts = Contact.objects.filter(show=True)
-context = {
-    'contacts': contacts,
-}
 
 def index(request):
+
+    contacts = Contact.objects.filter(show=True)
+    context = {
+        'contacts': contacts,
+    }
+    
     return render(request, 'contact/index.html', context)
+
+def single_contact(request, contact_id):
+
+    contact = get_object_or_404(Contact, id=contact_id)
+    context = {
+        'contacts': contact,
+    }
+    
+    return render(request, 'contact/single_contact.html', context)
