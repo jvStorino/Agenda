2f9a87f11d004e1248f2fb54fd6cde2b265e5f59
jvStorino <storinojv@gmail.com>
2026-03-19 16:41:49 -0300

    Programando busca de contato

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,6 @@ app_name = 'contact'
 
 urlpatterns = [
     path('<int:contact_id>/', views.single_contact, name='single_contact'),
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
--- contact/views/contacts.py
+++ contact/views/contacts.py
@@ -1,21 +1,46 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 
 def index(request):
 
     contacts = Contact.objects.filter(show=True)[:10]
     context = {
-        'contacts': contacts,
+        "contacts": contacts,
     }
-    
-    return render(request, 'contact/index.html', context)
+
+    return render(request, "contact/index.html", context)
+
+
+def search(request):
+    search_value = request.GET.get("q", "").strip()
+
+    if search_value == "":
+        return redirect("contact:index")
+
+    contacts = (
+        Contact.objects.filter(show=True)
+        .filter(
+            Q(first_name__icontains=search_value)
+            | Q(last_name__icontains=search_value)
+            | Q(email__icontains=search_value)
+            | Q(phone__icontains=search_value)
+        )
+    )
+
+    context = {
+        "contacts": contacts,
+    }
+
+    return render(request, "contact/index.html", context)
+
 
 def single_contact(request, contact_id):
 
     contact = get_object_or_404(Contact, id=contact_id)
     context = {
-        'contacts': contact,
+        "contacts": contact,
     }
-    
-    return render(request, 'contact/single_contact.html', context)
+
+    return render(request, "contact/single_contact.html", context)
