35c5abc5ccea521333d0ca5c87e9e864c8846bee
jvStorino <storinojv@gmail.com>
2026-04-14 18:22:15 -0300

    Add campos no form de create

--- contact/forms.py
+++ contact/forms.py
@@ -3,9 +3,13 @@ from django.core.exceptions import ValidationError
 from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
+    # first_name = forms.CharField(label = 'Primeiro nome')
+    # last_name = forms.CharField(label = 'Sobrenome')
+    # phone = forms.CharField(label = 'Telefone')
+
     class Meta:
         model = Contact
-        fields = ['first_name', 'last_name', 'phone']
+        fields = ['first_name', 'last_name', 'phone', 'email', 'description', 'category']
     
     def clean(self):
         # cleaned_data = self.cleaned_data
--- contact/models.py
+++ contact/models.py
@@ -20,15 +20,15 @@ class Category(models.Model):
         return self.name
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField(max_length=254)
+    first_name = models.CharField(max_length=50, verbose_name='Primeiro nome')
+    last_name = models.CharField(max_length=50, verbose_name='Sobrenome')
+    phone = models.CharField(max_length=20, verbose_name='Telefone')
+    email = models.EmailField(max_length=254, verbose_name='E-mail')
     created_date = models.DateTimeField(default=timezone.now)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, verbose_name='Descrição')
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='pictures/%Y/%m', blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Categorias')
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
