56bbab4b891eb3f5859b016a35ffe33adb43b4b7
jvStorino <storinojv@gmail.com>
2026-04-27 19:53:22 -0300

    adicionando funcionalidade delete

--- contact/urls.py
+++ contact/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
+
 from contact import views
 
-app_name = 'contact'
+app_name = "contact"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('search/', views.search, name='search'),
-
+    path("", views.index, name="index"),
+    path("search/", views.search, name="search"),
     # CRUD contact
-    path('contact/<int:contact_id>/', views.single_contact, name='single_contact'),
-    path('contact/<int:contact_id>/update/', views.update_contact, name='update_contact'),
-    path('contact/create/', views.create_contact, name='create_contact'),
+    path("contact/<int:contact_id>/", views.single_contact, name="single_contact"),
+    path(
+        "contact/<int:contact_id>/update/", views.update_contact, name="update_contact"
+    ),
+    path(
+        "contact/<int:contact_id>/delete/", views.delete_contact, name="delete_contact"
+    ),
+    path("contact/create/", views.create_contact, name="create_contact"),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,8 +1,8 @@
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
-from contact.models import Contact
 from contact.forms import ContactForm
+from contact.models import Contact
 
 
 def create_contact(request):
@@ -14,23 +14,18 @@ def create_contact(request):
         if form.is_valid():
             contact = form.save()
             return redirect("contact:update_contact", contact_id=contact.pk)
-        
-        context = {
-            'form': form,
-            'form_action': form_action
-        }
+
+        context = {"form": form, "form_action": form_action}
 
         return render(request, "contact/create.html", context)
 
-    context = {
-        'form': ContactForm(),
-        'form_action': form_action
-    }
+    context = {"form": ContactForm(), "form_action": form_action}
 
     return render(request, "contact/create.html", context)
 
+
 def update_contact(request, contact_id):
-    contact = get_object_or_404(Contact, pk=contact_id, show = True)
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
     form_action = reverse("contact:update_contact", args=(contact_id,))
 
     if request.method == "POST":
@@ -38,18 +33,24 @@ def update_contact(request, contact_id):
 
         if form.is_valid():
             contact = form.save()
-            return redirect("contact:update_contact", contact_id=contact.pk)
-        
-        context = {
-            'form': form,
-            'form_action': form_action
-        }
+            return redirect("contact:single_contact", contact_id=contact.pk)
+
+        context = {"form": form, "form_action": form_action}
 
         return render(request, "contact/create.html", context)
 
-    context = {
-        'form': ContactForm(instance=contact),
-        'form_action': form_action
-    }
+    context = {"form": ContactForm(instance=contact), "form_action": form_action}
 
     return render(request, "contact/create.html", context)
+
+
+def delete_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    confirmation = request.POST.get("confirmation", "no")
+
+    if confirmation == "yes":
+        contact.delete()
+        return redirect("contact:index")
+
+    context = {"contact": contact, "confirmation": confirmation}
+    return render(request, "contact/single_contact.html", context)
